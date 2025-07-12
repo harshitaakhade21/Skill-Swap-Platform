25d44fedd0094b8c72979fa24429a940bae689a4
harshita <harshitaakhade21@gmail.com>
2025-07-12 10:21:18 +0530

    Final update to app.py before demo

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import json
 import os
 
+from sympy import public
+
 #path to local JSON storage
 DATA_FILE = "data/users.josn"
 
@@ -30,4 +32,42 @@ skills_offered = st.sidebar.text_input("Skills you offer (comma-separated)")
 skills_wanted = st.sidebar.text_input("Skills you want (comma-separated)")
 availability = st.sidebar.text_input("Availability", ["weekdays", "weekends","Evening", "Flexible"])
 profile_photo = st.sidebar.checkbox("Make my profile public", value=True)
+
+if st.didebar.button("Submit Profile"):
+    if name and skills_offered and skills_wanted:
+        user_data = {
+            "name": name,
+            "skills_offered": [s.strip() for s in skills_offered.split(",")],
+            "skills_wanted": [s.strip() for s in skills_wanted.split(",")],
+            "availability": availability,
+            "public": profile_photo
+            
+        }
+        save_user(user_data)
+        st.success("Profile submitted successfully!")
+    else:
+        st.warning("Please fill in all fields before submitting.")            
+                         
+    # Main Section: View Profiles
+st.subheader("Browse Public Profiles by Skill")
+
+search_skill = st.text_input("Search by skill (e.g., Python, Design)")
+users = load_users()
+
+# Filter by search
+filtered_users = []
+for user in users:
+    if user["public"] and (
+        search_skill.lower() in [s.lower() for s in user["skills_offered"]] or search_skill == ""):
+        filtered_users.append(user)
+
+if filtered_users:
+    for user in filtered_users:
+        st.markdown(f"** {user['name']}**")
+        st.markdown(f" *Offers:* {', '.join(user['skills_offered'])}")
+        st.markdown(f" *Wants:* {', '.join(user['skills_wanted'])}")
+        st.markdown(f"*Availability:* {user['availability']}")
+        st.divider()
+else:
+    st.info("No matching profiles found. Try another skill or create one!")
 
